9e2978d9d72fad1d8c675cd7f35c33549105a872
kr0nverk <kr0nverk@yandex.ru>
2023-11-15 17:16:39 +0300

    Setup

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import pygame
 import sys
 
 from settings import *
+from  level import Level
 
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Valley')
         self.clock = pygame.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -18,6 +21,7 @@ class Game:
                     sys.exit()
 
             dt = self.clock.tick() / 1000
+            self.level.run(dt)
             pygame.display.update()
 
 
