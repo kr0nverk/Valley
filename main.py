3134e34341c94f80406eed36410008a5dd79a0a6
kr0nverk <kr0nverk@yandex.ru>
2023-11-15 15:28:41 +0300

    git init

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import pygame
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from settings import *
 
 
-# Press the green button in the gutter to run the script.
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            pygame.display.update()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    game = Game()
+    game.run()
---
+++ settings.py
@@ -0,0 +1,4 @@
+from pygame.math import Vector2
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
