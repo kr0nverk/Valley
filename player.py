1f9d6c51331780f167dacce52db8d90faeb61c78
kr0nverk <kr0nverk@yandex.ru>
2023-11-16 16:51:06 +0300

    normalizing movement

--- player.py
+++ player.py
@@ -12,6 +12,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=pos)
 
         self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -30,7 +32,17 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
-        print(self.direction)
+    def move(self, dt):
+
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
 
     def update(self, dt):
         self.input()
+        self.move(dt)
