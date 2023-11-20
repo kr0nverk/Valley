3b5c690abba9e5d3e319048cc84a0c89aac5f499
kr0nverk <kr0nverk@yandex.ru>
2023-11-20 16:09:00 +0300

    import graphics

---
+++ support.py
@@ -0,0 +1,13 @@
+from os import walk
+import pygame
+
+def import_folder(path):
+    surface_list = []
+
+    for folder_name, sub_folder, img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+
+    return  surface_list
